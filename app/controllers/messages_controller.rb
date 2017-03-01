476f6f6932f68d6f9adcafb53ea3e843c394b3f8
quan <thanhquan.ng@gmail.com>
2017-03-01 13:37:38 +0700

    Change read/unread status when user view message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,13 @@
 class MessagesController < ApplicationController
   def show
     @message = Message.find(params[:id])
+    if @message.presence
+      # update the status from unread to read
+      # 1: Read
+      # Other value is unread :)
+      @message.status = 1
+      @message.save!
+    end
   end
 
   def index
