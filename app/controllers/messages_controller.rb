d162fa55d1b4347575dc8cf2bcce498c40a8b027
quan <thanhquan.ng@gmail.com>
2017-03-03 16:00:25 +0700

    Allow user to send messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -32,5 +32,22 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @message = Message.new(message_params)
+    @message.sender_id = current_user.id
+
+    respond_to do |format|
+      if @message.save
+        format.html { redirect_to root_path, notice: 'Message was successfully sent.' }
+      else
+        format.html { render :new }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
+    end 
   end
+
+  private
+  def message_params
+    params.require(:message).permit(:subject, :body, :receiver_id)
+  end
+
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
+  validates :sender_id, :receiver_id, presence: true
+
   def get_sender
     User.find(sender_id)
   end
